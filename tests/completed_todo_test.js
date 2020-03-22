78b1b6be1e91f77dc9bbf9dba9d953ff7beb87ae
Ksu <rallyt789@gmail.com>
2020-03-23 00:12:25 +0200

    tests for completed todo added

---
+++ tests/completed_todo_test.js
@@ -0,0 +1,28 @@
+Feature('Completed todo');
+
+Before((I) => {
+	I.amOnPage('http://todomvc.com/examples/typescript-angular');
+});
+
+Scenario('mark todo item as completed', async (I, tdmvcPage) => {
+	tdmvcPage.createTd();
+	I.click(tdmvcPage.completedCheckbox);
+	I.seeElement(tdmvcPage.completedTd);
+});
+
+Scenario('mark completed todo as active', async (I, tdmvcPage) => {
+	tdmvcPage.createTd();
+	I.click(tdmvcPage.completedCheckbox);
+	I.seeElement(tdmvcPage.completedTd);
+	I.click(tdmvcPage.completedCheckbox);
+	I.dontSeeElement(tdmvcPage.completedTd);
+});
+
+Scenario('test clear completed button', async (I, tdmvcPage) => {
+	tdmvcPage.createMultipleTd();
+	const allTd = await I.grabNumberOfVisibleElements(tdmvcPage.tdInList);
+	I.click(tdmvcPage.completedCheckbox);
+	const completedTd = await I.grabNumberOfVisibleElements(tdmvcPage.completedTd);
+	I.click(tdmvcPage.clearCompletedButton);
+	I.seeNumberOfVisibleElements(tdmvcPage.tdInList, allTd - completedTd);
+});
