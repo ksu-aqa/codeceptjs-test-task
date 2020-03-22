d816d751b26c1f96814c76b27dcdda0f8336cb04
Ksu <rallyt789@gmail.com>
2020-03-23 00:25:07 +0200

    indentation fixed

--- steps_file.js
+++ steps_file.js
@@ -2,12 +2,12 @@
 module.exports = function() {
   return actor({
 
-    updateField(fieldName, value) {
-    	this.appendField(fieldName, '');
-    	this.pressKey(['Shift', 'Home']);
-    	this.pressKey('Backspace');
-    	this.appendField(fieldName, value);
-    	this.pressKey("Enter");
+  updateField(fieldName, value) {
+    this.appendField(fieldName, '');
+    this.pressKey(['Shift', 'Home']);
+    this.pressKey('Backspace');
+    this.appendField(fieldName, value);
+    this.pressKey("Enter");
     }
 
   });
